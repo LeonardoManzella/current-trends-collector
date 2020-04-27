061ac56b76d7da13f63b2ffc6d80a39b15606adc
LeonardoManzella <leonardo.manzella@gmail.com>
2020-04-27 12:16:02 -0300

    Added comments

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+// EntryPoint for Cloudflare
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
-})
+});
+
+//Handle Main Request
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
+  return new Response('Hello worker response!', {
     headers: { 'content-type': 'text/plain' },
   });
 }
