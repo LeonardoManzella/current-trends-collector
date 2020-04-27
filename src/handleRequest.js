3188adefe086cff37e2e861c49ca440d6ef4433e
LeonardoManzella <leonardo.manzella@gmail.com>
2020-04-27 18:19:20 -0300

    Working Google Trends Api

--- src/handleRequest.js
+++ src/handleRequest.js
@@ -9,6 +9,7 @@
 // importScripts("workerFakeDOM.js");
 // importScripts('jquery-3.3.1');
 // const htmlparser2 = require("htmlparser2");
+import {obtainGoogleTrendsArg, obtainGoogleTrendsGlobal} from './google-analizer';
 
 const IFTTT_WEBHOOK_URL = 'https://maker.ifttt.com/trigger/estado_transito/with/key/bEJDjvRQ04PPYZVlKAP2E8';
 
@@ -20,31 +21,34 @@ export default async function handleRequest(mainRequest) {
     //TODO make basic responsive html table (copy from transportations-changes project), in another JS file
 
     //TODO use Google API to generate daily trend news, in another JS file
+    const googleTrendsArg = await obtainGoogleTrendsArg();
+    const googleTrendsGlobal = await obtainGoogleTrendsGlobal();
 
     //TODO parse Twitter analizer page to generate twitter daily trending topics section, in another JS file
 
     console.group('Calling IFTTT');
-    await fetch(IFTTT_WEBHOOK_URL, {
-        body: JSON.stringify({ 
-            value1: "email",
-            value2: "title",
-            value3: "optional value",
-            }),
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-    })
-    .then(function (response) {
-        console.log("Post send, response: ", response);
-    })
-    .catch(function (error) {
-        console.error('--- Error Detected ---');
-        console.error(error);
-        console.trace();
-    })
-    .finally(function () {
-        // always executed
-        console.groupEnd();
-    });
+    // TODO uncomment to publish
+    // await fetch(IFTTT_WEBHOOK_URL, {
+    //     body: JSON.stringify({ 
+    //         value1: "email",
+    //         value2: "title",
+    //         value3: "optional value",
+    //         }),
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    // })
+    // .then(function (response) {
+    //     console.log("Post send, response: ", response);
+    // })
+    // .catch(function (error) {
+    //     console.error('--- Error Detected ---');
+    //     console.error(error);
+    //     console.trace();
+    // })
+    // .finally(function () {
+    //     // always executed
+    //     console.groupEnd();
+    // });
 
     return new Response('Everything worked!', {
         headers: { 'content-type': 'text/plain' },
